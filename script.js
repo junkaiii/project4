511d7c8fa34ef259489f9b20c25aba84ed522527
Jun Kai Lim <junkaiii@gmail.com>
2016-09-10 12:50:14 +0800

    improved boiler plate with some refactoring

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
 $(function() {
 
-//Create the renderer
-var renderer = PIXI.autoDetectRenderer(500,500);
+//Aliases
+var Container = PIXI.Container,
+    autoDetectRenderer = PIXI.autoDetectRenderer,
+    loader = PIXI.loader,
+    reources = PIXI.loader.resources,
+    Sprite = PIXI.Sprite;
 
-//Create a container object called the stage
-var stage = new PIXI.Container();
+//Create a stage & renderer add add it to the DOM
+var stage = new Container(),
+    renderer = PIXI.autoDetectRenderer(500,500);
 
-//Add the canvas to the HTML doucment
 $('body').append(renderer.view);
 
 //Loading images into the texture cache
-PIXI.loader
+loader
   .add("images/cat.png")
   .load(setup);
 
 function setup(){
-  //This code will run when the loader has finished loading the page
-  var cat = new PIXI.Sprite(
-    PIXI.loader.resources["images/cat.png"].texture
-  );
 
-  //Add the cat sprite to the stage
+  // //Converting images to sprite to render in renderer - long version
+  // var cat = new PIXI.Sprite(
+  //   PIXI.loader.resources["images/cat.png"].texture
+  // );
+
+  //Converting images to sprite to render in renderer - short version
+  var cat = new Sprite(resources["images/cat"].texture);
+
+  //Add the converted cat sprite texture to the stage
   stage.addChild(cat);
+
+  // //To remove the cat sprite from stage
+  // stage.removeChild(anySprite);
+
+  // //To change the cat sprite to invisible
+  // cat.visible = false;
 
   //Tell the renderer to render the stage
   renderer.render(stage);
