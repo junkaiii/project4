e6d06aafeea13d265c6fd54ee819ae306405a3f9
Jun Kai Lim <junkaiii@gmail.com>
2016-09-10 12:30:46 +0800

    basic picir boilerplate with one sprite

--- script.js
+++ script.js
@@ -3,14 +3,32 @@ $(function() {
 //Create the renderer
 var renderer = PIXI.autoDetectRenderer(500,500);
 
-//Add the canvas to the HTML doucment
-$('body').append(renderer.view);
-
 //Create a container object called the stage
 var stage = new PIXI.Container();
 
-//Tell the renderer to render the stage
-renderer.render(stage);
+//Add the canvas to the HTML doucment
+$('body').append(renderer.view);
+
+//Loading images into the texture cache
+PIXI.loader
+  .add("images/cat.png")
+  .load(setup);
+
+function setup(){
+  //This code will run when the loader has finished loading the page
+  var cat = new PIXI.Sprite(
+    PIXI.loader.resources["images/cat.png"].texture
+  );
+
+  //Add the cat sprite to the stage
+  stage.addChild(cat);
+
+  //Tell the renderer to render the stage
+  renderer.render(stage);
+
+}
+
+
 
 
 });
